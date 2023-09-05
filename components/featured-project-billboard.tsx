5b4e33a9b7567e3447c1b7cd15db7a1c79de8e55
Manuel Rodriguez Urdapilleta <manuelrurda@gmail.com>
2023-09-05 11:12:24 -0600

    fetch featured project

--- components/featured-project-billboard.tsx
+++ components/featured-project-billboard.tsx
@@ -1,31 +1,26 @@
 import Image from "next/image";
 
-const FeaturedProjectBillboard = () => {
+import getFeaturedProjectData from "@/actions/get-featured-project";
+
+const FeaturedProjectBillboard = async () => {
+  const featuredProject = await getFeaturedProjectData();
+
   return (
     <div className="p-10">
-      <h1 className="font-bold text-2xl pb-3">Project Name</h1>
+      <h1 className="font-bold text-3xl pb-3">{featuredProject?.name}</h1>
       <div className="flex flex-col gap-x-7 lg:flex-row items-center">
         <Image
-          src="https://github.com/manuelrurda/Hangouts/assets/62727899/c7dc473d-6f19-415d-845f-99e792445142"
-          alt=""
+          src={featuredProject?.thumbnailUrl}
+          alt={featuredProject?.name}
           width="500"
           height="200"
           className="rounded-md max-h-80 my-3"
         />
         <div className="flex-1 flex flex-col min-w-mi self-start">
           <p className="text-gray-400 my-3">
-            Android • Java • Kotlin • Firebase
+            {featuredProject?.techTags.join(" • ")}
           </p>
-          <p>
-            Hangouts is an event/social planner app that allows users to create
-            events or "Hangouts" and share them with other users. Then, said
-            users can join the event and rank their top picks for meal
-            selection. The app then will run an algorithm to process each
-            members preferences and calculate which restaurants might be a good
-            fit for the member's current cravings. Finally, the app will display
-            those restaurants on a map so participants can make a decision on
-            where to go.
-          </p>
+          <p className="text-justify">{featuredProject?.description}</p>
         </div>
       </div>
     </div>
