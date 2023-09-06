5a5c2a2d9930bf20b189e56ed5b9123a7436a3bc
Manuel Rodriguez Urdapilleta <manuelrurda@gmail.com>
2023-09-06 12:06:23 -0600

    Added on hover links for project cards

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,3 +1,4 @@
+import { Github, Link } from "lucide-react";
 import Image from "next/image";
 
 interface ProjectCardProps {
@@ -18,16 +19,44 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   liveUrl,
 }) => {
   return (
-    <div className="shadow-lg inline-block min-w-[300px] max-w-md rounded-md min-h h-full">
-      <div className="flex flex-col justify-center align-middle p-5 gap-y-3">
+    <div className="shadow-lg inline-block min-w-[300px] max-w-md rounded-md min-h h-full group">
+      <div className="flex flex-col justify-start align-middle p-5 gap-y-3 w-fit h-full">
         <h1 className="text-xl font-bold">{name}</h1>
-        <Image
-          src={imageSrc}
-          alt="img"
-          width="200"
-          height="200"
-          className="min-w-full rounded-md"
-        />
+        <div className="relative">
+          <div className="after:w-full after:h-full after:left-0 after:top-0 after:absolute after:rounded-md group-hover:after:bg-white group-hover:after:opacity-70 transition">
+            <Image
+              src={imageSrc}
+              alt="img"
+              width="200"
+              height="200"
+              className="min-w-full h-full rounded-md"
+            />
+          </div>
+
+          <div className="opacity-0 group-hover:opacity-80 transition absolute w-1/2 h-1/2 top-1/4 left-1/4">
+            <div className="flex gap-x-6 justify-center">
+              {githubUrl && (
+                <a href={githubUrl} target="_blank">
+                  <Github
+                    className="bg-black p-2 rounded-xl mx-1 cursor-pointer"
+                    color="white"
+                    size={55}
+                  />
+                </a>
+              )}
+
+              {liveUrl && (
+                <a href={liveUrl} target="_blank">
+                  <Link
+                    className="bg-black p-2 rounded-xl mx-1 cursor-pointer"
+                    color="white"
+                    size={55}
+                  />
+                </a>
+              )}
+            </div>
+          </div>
+        </div>
         <p className="text-gray-400">{techTags.join(" • ")}</p>
         <p className="text-justify">{description}</p>
       </div>
