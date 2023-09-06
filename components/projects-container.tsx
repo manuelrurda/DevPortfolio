3b844fb6eb46553241b37fb3b1b11bda801056a9
Manuel Rodriguez Urdapilleta <manuelrurda@gmail.com>
2023-09-06 17:43:56 -0600

    fixed missing key prop

--- components/projects-container.tsx
+++ components/projects-container.tsx
@@ -7,6 +7,7 @@ const ProjectsContainer = async () => {
     <div className="mx-5 grid gap-7 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center place-content-center auto-rows-max">
       {projects?.map((project) => (
         <ProjectCard
+          key={project.githubUrl}
           name={project.name}
           imageSrc={project.thumbnailUrl}
           techTags={project.techTags}
