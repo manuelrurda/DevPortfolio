10bfbb125a6c3aeaab31c9575f4de78e7ce4b8b3
Manuel Rodriguez Urdapilleta <manuelrurda@gmail.com>
2023-09-06 16:10:14 -0600

    Footer link

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,8 +2,8 @@ import { Github } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className="bg-white border-t mt-5 flex flex-row align-middle justify-center">
-      <div className="mx-auto py-7">
+    <footer className="bg-white border-t mt-10 flex flex-row align-middle justify-center">
+      <div className="mx-auto py-5">
         <p className="text-center text-xs text-gray-500">
           Manuel Rodriguez Urdapilleta
         </p>
@@ -12,8 +12,8 @@ const Footer = () => {
         </p>
         <p className="text-center text-xs text-gray-500">MX 5534552933</p>
       </div>
-      <div className="py-5">
-        <a href="" target="_blank">
+      <div className="absolute self-center right-1 mx-5">
+        <a href="https://github.com/manuelrurda/DevPortfolio" target="_blank">
           <Github
             className=" bg-black opacity-20 p-2 rounded-xl mx-1 cursor-pointer"
             color="white"
